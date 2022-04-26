21662fd2073e3118e3666059f63123a8ea1a59df
Junior06sre <juniorsanchezzz06@gmail.com>
2022-04-26 11:32:22 -0400

    Add project files.

---
+++ bookmedik-win/Detailsform.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace bookmedik_win
+{
+    partial class Detailsform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.nose = new System.Windows.Forms.Label();
+            this.nose2 = new System.Windows.Forms.Label();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.ofdSeleccionar = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(247, 350);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(128, 28);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.Location = new System.Drawing.Point(55, 350);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(128, 28);
+            this.btnSeleccionar.TabIndex = 1;
+            this.btnSeleccionar.Text = "Seleccionar";
+            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            this.btnSeleccionar.Click += new System.EventHandler(this.btnSeleccionar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(293, 45);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(82, 33);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // nose
+            // 
+            this.nose.AutoSize = true;
+            this.nose.Location = new System.Drawing.Point(50, 45);
+            this.nose.Name = "nose";
+            this.nose.Size = new System.Drawing.Size(21, 13);
+            this.nose.TabIndex = 3;
+            this.nose.Text = "ID:";
+            // 
+            // nose2
+            // 
+            this.nose2.AutoSize = true;
+            this.nose2.Location = new System.Drawing.Point(52, 78);
+            this.nose2.Name = "nose2";
+            this.nose2.Size = new System.Drawing.Size(47, 13);
+            this.nose2.TabIndex = 4;
+            this.nose2.Text = "Nombre:";
+            // 
+            // pbImagen
+            // 
+            this.pbImagen.Location = new System.Drawing.Point(55, 104);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(320, 240);
+            this.pbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImagen.TabIndex = 5;
+            this.pbImagen.TabStop = false;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(107, 42);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(150, 20);
+            this.txtId.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(107, 75);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(150, 20);
+            this.txtNombre.TabIndex = 7;
+            // 
+            // ofdSeleccionar
+            // 
+            this.ofdSeleccionar.FileName = "openFileDialog1";
+            // 
+            // Detailsform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 405);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.pbImagen);
+            this.Controls.Add(this.nose2);
+            this.Controls.Add(this.nose);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnSeleccionar);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "Detailsform";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Detailsform_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnSeleccionar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label nose;
+        private System.Windows.Forms.Label nose2;
+        private System.Windows.Forms.PictureBox pbImagen;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.OpenFileDialog ofdSeleccionar;
+    }
+}
---
+++ bookmedik-win/Detailsform.cs
@@ -0,0 +1,100 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bookmedik_win
+{
+    public partial class Detailsform : Form
+    {
+        public Detailsform()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            string sql = "SELECT nombre, imagen FROM foto WHERE id = '" + id + "'";
+            Connection conexionBD = new Connection();
+            conexionBD.con.Open();
+
+            try
+            {
+                MySqlCommand comando = conexionBD.con.CreateCommand();
+                comando.CommandText = "SELECT nombre, imagen FROM fotos WHERE id = '" + id + "'";
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    MemoryStream ms = new MemoryStream((byte[])reader["imagen"]);
+                    Bitmap mb = new Bitmap(ms);
+                    pbImagen.Image = mb;
+                    txtNombre.Text = reader["nombre"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("No se encontraron registros");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al buscar imagen: " + ex.Message);
+            }
+        }
+
+        private void Detailsform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdSeleccionar = new OpenFileDialog();
+            ofdSeleccionar.Filter = "Imagenes|*.jpg; *.png";
+            ofdSeleccionar.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            ofdSeleccionar.Title = "Seleccionar imagen";
+
+            if (ofdSeleccionar.ShowDialog() == DialogResult.OK)
+            {
+                pbImagen.Image = Image.FromFile(ofdSeleccionar.FileName);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+            pbImagen.Image.Save(ms, ImageFormat.Jpeg);
+            byte[] aBytes = ms.ToArray();
+
+            Connection conexionBD = new Connection();
+            conexionBD.con.Open();
+
+            try
+            {
+                MySqlCommand comando = conexionBD.con.CreateCommand();
+                comando.CommandText = "INSERT INTO fotos (nombre, imagen) VALUES ('" + txtNombre.Text + "', @imagen)";
+                comando.Parameters.AddWithValue("imagen", aBytes);
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Imagen guardada");
+                pbImagen.Image = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar imagen: " + ex.Message);
+            }
+
+            txtNombre.Text = null;
+        }
+    }
+}
